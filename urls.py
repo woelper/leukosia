fcc61754b9a85e1874134cffcf711b5418db4057
Johann Woelper <woelper@gmail.com>
2013-02-16 23:13:05 +0100

    first commit

---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'leukosia.views.home', name='home'),
+    # url(r'^leukosia/', include('leukosia.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^radiostations/', 'webfrontend.views.radiostations'),
+    url(r'^playqueue/', 'webfrontend.views.playqueue'),
+    url(r'^nowplaying/', 'webfrontend.views.nowplaying'),
+    url(r'^lastfmalbuminfo/', 'webfrontend.views.get_lastfm_albuminfo'),
+    url(r'^lastfmartistinfo/', 'webfrontend.views.get_lastfm_artistinfo'),
+    url(r'^login/', 'webfrontend.views.login'),
+    url(r'^mpdcommands/', 'webfrontend.views.mpdstop'),
+)
